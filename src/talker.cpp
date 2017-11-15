62a177c9f1a761e5b5aeb676cb0da50d4445a0ec
harish <harish1696@gmail.com>
2017-11-14 22:16:29 -0500

    Adding License in the source files

--- src/talker.cpp
+++ src/talker.cpp
@@ -1,4 +1,38 @@
 /**
+ * BSD 3-Clause LICENSE
+ *
+ * Copyright <2017> <HARISH SAMPATHKUMAR>
+ *
+ * Redistribution and use in source and binary forms, with or without  
+ * modification, are permitted provided that the following conditions are 
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, 
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ * notice, this list of conditions and the following disclaimer in the   
+ * documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its 
+ * contributors may be used to endorse or promote products derived from this 
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+ * CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
  *  @file    talker.cpp
  *  @author  Harish Sampathkumar
  *  @copyright BSD License
@@ -60,6 +94,7 @@ int main(int argc, char **argv) {
    */
   ros::NodeHandle n;
 
+  // Creating objects of class TransformBroadcaster and Transform
   tf::TransformBroadcaster br;
   tf::Transform transform;
 
@@ -116,11 +151,13 @@ int main(int argc, char **argv) {
      * This is a message object. You stuff it with data, and then publish it.
      */
     std_msgs::String msg;
-
+    
+    // setting the origin and rotation for the transform object
     transform.setOrigin( tf::Vector3(2.0,3.0,0.0));
     tf::Quaternion q;
     q.setRPY(0, 0, 6.28);
     transform.setRotation(q);
+    // braoadcasting the transform using Transformbroadcaster
     br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "talk"));
 
     std::stringstream ss;
